307b2c4a5aa4e1b2d0783606b394b27c0e893d76
Pramod Bharti <pramodbharti.info@gmail.com>
2025-07-07 19:59:22 +0530

    resolved erros

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -10,13 +10,18 @@ repositories {
     mavenCentral()
 }
 
+java{
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    implementation("software.amazon.awscdk:aws-cdk-lib:2.204.0")
-    implementation("com.amazonaws:aws-java-sdk:2.31.77")
+    implementation("software.amazon.awscdk:aws-cdk-lib:2.178.1")
+    implementation("com.amazonaws:aws-java-sdk:1.12.780")
 }
 
 tasks.test {
--- infrastructure/src/main/java/com/db/stack/LocalStack.java
+++ infrastructure/src/main/java/com/db/stack/LocalStack.java
@@ -1,7 +1,22 @@
 package com.db.stack;
 
-import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.*;
 
 public class LocalStack extends Stack {
+
+    public LocalStack(final App scope, final String id, final StackProps props) {
+        super(scope, id, props);
+    }
+
+    public static void main(final String[] args) {
+        final App app = new App(AppProps.builder().outdir("./cdk.out").build());
+        StackProps props = StackProps
+                .builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+        new LocalStack(app, "localstack", props);
+        app.synth();
+        System.out.println("cdk synth successful");
+    }
 
 }
