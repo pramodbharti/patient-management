f1376b2d55b3fdebf258de74a682af4e1d8bc9a3
Pramod Bharti <pramodbharti.info@gmail.com>
2025-07-08 06:49:26 +0530

    fargate service method implemented

--- infrastructure/src/main/java/com/db/stack/LocalStack.java
+++ infrastructure/src/main/java/com/db/stack/LocalStack.java
@@ -3,12 +3,17 @@ package com.db.stack;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ecs.CloudMapNamespaceOptions;
-import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.Protocol;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class LocalStack extends Stack {
@@ -29,6 +34,8 @@ public class LocalStack extends Stack {
         CfnCluster mskCluster = createMskCluster();
 
         this.ecsCluster = createEcsCluster();
+
+
 
     }
 
@@ -100,6 +107,76 @@ public class LocalStack extends Stack {
                                 .collect(Collectors.toList()))
                         .brokerAzDistribution("DEFAULT")
                         .build())
+                .build();
+    }
+
+    private FargateService createFargateService(
+            String id,
+            String imageName,
+            List<Integer> ports,
+            DatabaseInstance db,
+            Map<String, String> envVars) {
+
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition
+                .Builder
+                .create(this, id + "Task")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        ContainerDefinitionOptions.Builder containerOptions = ContainerDefinitionOptions
+                .builder()
+                .image(ContainerImage.fromRegistry(imageName))
+                .portMappings(ports
+                        .stream()
+                        .map(port -> PortMapping
+                                .builder()
+                                .containerPort(port)
+                                .hostPort(port)
+                                .protocol(Protocol.TCP)
+                                .build())
+                        .toList())
+                .logging(LogDriver
+                        .awsLogs(AwsLogDriverProps
+                                .builder()
+                                .logGroup(LogGroup
+                                        .Builder
+                                        .create(this, id + "LogGroup")
+                                        .logGroupName("/ecs/" + imageName)
+                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                        .retention(RetentionDays.ONE_DAY)
+                                        .build())
+                                .build()));
+
+        Map<String, String> containerEnvVars = new HashMap<>();
+        containerEnvVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511, localhost.localstack.cloud:4512");
+        if (envVars != null) {
+            containerEnvVars.putAll(envVars);
+        }
+
+        if (db != null) {
+            containerEnvVars.put("SPRING_DATASOURCE_URL", "jdbc:postgresql://%s:%s/%s-db".formatted(
+                    db.getDbInstanceEndpointAddress(),
+                    db.getDbInstanceEndpointPort(),
+                    imageName
+            ));
+            containerEnvVars.put("SPRING_DATASOURCE_USERNAME", "admin_user");
+            containerEnvVars.put("SPRING_DATASOURCE_PASSWORD", db.getSecret().secretValueFromJson("password").toString());
+            containerEnvVars.put("SPRING_JPA_HIBERNATE_DDL_AUTO", "update");
+            containerEnvVars.put("SPRING_SQL_INIT_MODE", "always");
+            containerEnvVars.put("SPRING_DATASOURCE_HIKARI_INITIALIZATION_FAIL_TIMEOUT", "60000");
+        }
+
+        containerOptions.environment(containerEnvVars);
+        taskDefinition.addContainer(imageName + "Container", containerOptions.build());
+
+        return FargateService
+                .Builder
+                .create(this, id)
+                .cluster(ecsCluster)
+                .taskDefinition(taskDefinition)
+                .assignPublicIp(false)
+                .serviceName(imageName)
                 .build();
     }
 
