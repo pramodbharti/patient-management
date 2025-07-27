f4ab484b3f39ba51464d65afa40a1bcf1f374937
Pramod Bharti <pramodbharti.info@gmail.com>
2025-07-27 07:30:52 +0530

    added entities

---
+++ appointment-service/src/main/java/com/db/appointmentservice/entity/Appointment.java
@@ -0,0 +1,98 @@
+package com.db.appointmentservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull(message = "Patient id is required")
+    @Column(nullable = false)
+    private UUID patientId;
+
+    @NotNull(message = "startTime is required")
+    @Column(nullable = false)
+    @Future(message = "startTime must be in the future")
+    private LocalDateTime startTime;
+
+    @NotNull(message = "endTime is required")
+    @Column(nullable = false)
+    @Future(message = "endTime must be in the future")
+    private LocalDateTime endTime;
+
+    @NotNull(message = "Reason is required")
+    @Size(max = 255, message = "Reason can not exceed 255 characters")
+    @Column(nullable = false)
+    private String reason;
+
+    @Version
+    @Column(nullable = false)
+    private long version;
+
+    public Appointment(UUID patientId, LocalDateTime startTime, LocalDateTime endTime, String reason) {
+        this.patientId = patientId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.reason = reason;
+    }
+
+    public Appointment() {
+
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(UUID patientId) {
+        this.patientId = patientId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public long getVersion() {
+        return version;
+    }
+
+    public void setVersion(long version) {
+        this.version = version;
+    }
+}
