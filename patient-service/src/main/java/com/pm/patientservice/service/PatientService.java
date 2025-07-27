73e6d487bb5c9e975a509d34cae51da01c44423c
Pramod Bharti <pramodbharti.info@gmail.com>
2025-07-27 14:07:45 +0530

    kafka integration

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -78,7 +78,7 @@ public class PatientService {
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
 
-        kafkaProducer.sendEvent(newPatient);
+        kafkaProducer.sendPatientCreatedEvent(newPatient);
 
         return PatientMapper.toDTO(newPatient);
     }
@@ -96,6 +96,7 @@ public class PatientService {
         patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
 
         Patient updatedPatient = patientRepository.save(patient);
+        kafkaProducer.sendPatientUpdatedEvent(updatedPatient);
         return PatientMapper.toDTO(updatedPatient);
     }
 
